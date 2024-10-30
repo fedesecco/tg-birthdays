32ea4fea555fa522ceb4823f22855930a9848e7b
Federico Secco <federico.secco7@gmail.com>
2024-10-30 18:24:38 +0100

    Added firebase secrets

--- src/bot.ts
+++ src/bot.ts
@@ -1,26 +1,26 @@
-import { Bot, session, webhookCallback } from 'grammy';
-import express, { Request, Response, NextFunction } from 'express';
-import dotenv from 'dotenv';
-import { Commands, MyContext, Requests } from './enums';
-import { createClient } from '@supabase/supabase-js';
-import { addConversation, onAdd } from './commands/add';
-import { deleteConversation, onDelete } from './commands/delete';
-import { conversations, createConversation } from '@grammyjs/conversations';
-import { onTest } from './commands/test';
-import { onSubscribe } from './commands/subscribe';
-import { onUnsubscribe } from './commands/unsubscribe';
-import { onTestCron } from './requests/testCron';
-import { onBirthDaysOfTheDay } from './requests/bdaysOfTheDay';
-import { onToday } from './commands/today';
-import { onSearch, searchConversation } from './commands/search';
-import { Database } from './schema';
+import { Bot, session, webhookCallback } from "grammy";
+import express, { Request, Response, NextFunction } from "express";
+import dotenv from "dotenv";
+import { Commands, MyContext, Requests } from "./enums";
+import { createClient } from "@supabase/supabase-js";
+import { addConversation, onAdd } from "./commands/add";
+import { deleteConversation, onDelete } from "./commands/delete";
+import { conversations, createConversation } from "@grammyjs/conversations";
+import { onTest } from "./commands/test";
+import { onSubscribe } from "./commands/subscribe";
+import { onUnsubscribe } from "./commands/unsubscribe";
+import { onTestCron } from "./requests/testCron";
+import { onBirthDaysOfTheDay } from "./requests/bdaysOfTheDay";
+import { onToday } from "./commands/today";
+import { onSearch, searchConversation } from "./commands/search";
+import { Database } from "./schema";
 
 dotenv.config();
 
 // TELEGRAM BOT INIT
 const token = process.env.TELEGRAM_TOKEN;
 if (!token) {
-    console.error('No token!');
+    console.error("No token!");
 }
 export const bot = new Bot<MyContext>(token);
 /** conversations */
@@ -37,11 +37,11 @@ export const supabase = createClient<Database>(process.env.SUPABASE_URL, process
 });
 if (supabase.storage) {
     console.log(`Login successful.`);
-} else console.log('Fail on login');
+} else console.log("Fail on login");
 
 // start
 bot.command(Commands.start, (ctx) => {
-    console.log('/start triggered');
+    console.log("/start triggered");
     onAdd(ctx);
 });
 
@@ -55,14 +55,14 @@ bot.command(Commands.search, onSearch);
 
 // API calls from cyclic
 const onRequest = async (req: Request, res: Response, next: NextFunction) => {
-    if (req.method === 'POST' && req.path === Requests.bdays) {
+    if (req.method === "POST" && req.path === Requests.bdays) {
         await onBirthDaysOfTheDay();
         res.status(200);
-        res.send('done');
-    } else if (req.method === 'POST' && req.path === Requests.test) {
+        res.send("done");
+    } else if (req.method === "POST" && req.path === Requests.test) {
         await onTestCron();
         res.status(200);
-        res.send('done');
+        res.send("done");
     }
     next();
 };
@@ -73,11 +73,11 @@ const onRequest = async (req: Request, res: Response, next: NextFunction) => {
  */
 
 //deploy
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
     const app = express();
     app.use(express.json());
     app.use(onRequest);
-    app.use(webhookCallback(bot, 'express'));
+    app.use(webhookCallback(bot, "express"));
     /** listen */
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
